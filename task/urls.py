b8171fec94b3f37485df8185303f1035f18528fd
Naman Shah <nashah2001@gmail.com>
2021-02-14 01:39:14 +0530

    fixed isssues after adding author to task model

--- task/urls.py
+++ task/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import listTask, updateTask, deleteTask, statusTask
+from .views import listTask, updateTask, deleteTask, statusTask, TaskCreateView
 
 urlpatterns = [
-    path('', listTask, name='task-home'),
+    # path('', listTask, name='task-home'),
+    path('', TaskCreateView.as_view(), name='task-home'),
 	path('update/<int:pk>/', updateTask, name='task-update'),
 	path('delete/<int:pk>/', deleteTask, name='task-delete'),
 	path('task/<int:pk>/', statusTask, name='task-status'),
 ]
-
--- task/views.py
+++ task/views.py
@@ -1,21 +1,49 @@
-from django.shortcuts import render, HttpResponse, redirect
+from django.shortcuts import render, HttpResponse, redirect, reverse
 from django.contrib.auth.decorators import login_required
+from django.views.generic import CreateView, UpdateView, DeleteView
 from .models import Task
 from .forms import TaskForm
 
 # Create your views here.
 
-@login_required
+# @login_required
 def listTask(request):
 	tasks = Task.objects.all()
 	form = TaskForm()
 	if request.method == 'POST':
 		form = TaskForm(request.POST)
+		print('##### not validated #####')
+		print(request.user)
+		# print(form.instance.author)
+		print(form.instance.title)
+		print(form.instance.time)
+		form.instance.author = request.user
+		print(form.instance.author)
 		if form.is_valid():
 			form.save()
+			print('##### validated #####')
 			return redirect('/')
 	context = {'tasks': tasks, 'form': form}
 	return render(request, 'task/home.html', context)
+
+class TaskCreateView(CreateView):
+	model = Task
+	fields = ['title']
+	template_name = 'task/home.html'  # <app>/<model>_<viewtype>.html
+	context_object_name = 'posts'
+	extra_context = {}
+
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)
+	
+	def get_success_url(self):
+		return reverse('task-home')
+	
+	def get_context_data(self, *args, **kwargs):
+		extra_context = super(TaskCreateView, self).get_context_data(*args, **kwargs)
+		extra_context['tasks'] = Task.objects.all()
+		return extra_context
 
 def updateTask(request, pk):
 	task = Task.objects.get(id=pk)
